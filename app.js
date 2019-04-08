70edef3417d60e61281b0ee3bd568c34805aee61
Jessica  Gomez <jessicagomez@Jessicas-MacBook-Air.local>
2019-04-08 16:26:46 -0700

    seems to be working"

--- app.js
+++ app.js
@@ -1,71 +1,94 @@
- // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyDAtJ6aKRuyI2Rd_X0Ec6miO99q81s8cos",
-    authDomain: "plant-watering-schedule.firebaseapp.com",
-    databaseURL: "https://plant-watering-schedule.firebaseio.com",
-    projectId: "plant-watering-schedule",
-    storageBucket: "plant-watering-schedule.appspot.com",
-    messagingSenderId: "181471983769"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDAtJ6aKRuyI2Rd_X0Ec6miO99q81s8cos",
+  authDomain: "plant-watering-schedule.firebaseapp.com",
+  databaseURL: "https://plant-watering-schedule.firebaseio.com",
+  projectId: "plant-watering-schedule",
+  storageBucket: "plant-watering-schedule.appspot.com",
+  messagingSenderId: "181471983769"
+};
+firebase.initializeApp(config);
 
-  //saving the firebase database into a database variable
-  var database = firebase.database()
+//saving the firebase database into a database variable
+var database = firebase.database()
 
-  //preparing to add and event listener
-  //this button will add the contents of the user input
-  var addingPlant = document.getElementById('addBtn')
 
-  addingPlant.addEventListener('click', function(event){
-      console.log("i've been clicked!")
+var addingPlant = document.getElementById('addBtn')
 
-    //prevent default doesn't refresh the page
-    event.preventDefault()
 
-    var name = document.getElementById("plantName-input").value
-    console.log(name)
-    var lastWatering = document.getElementById("lastWatering-input").value
-    console.log(lastWatering)
-    var frequency = document.getElementById("frequency-input").value
-    console.log(frequency)
-    var nextWatering= 
+addingPlant.addEventListener('click', function (event) {
+  console.log("i've been clicked!")
+
+  //prevent default doesn't refresh the page
+  event.preventDefault()
+
+  var name = document.getElementById("plantName-input").value
+  console.log("plant name: "+name)
+
+  var lastWatering = moment(document.getElementById("lastWatering-input").value).format('LL')
+  console.log("last watered on " + lastWatering)
+
+  
+  
+  var frequency = document.getElementById("frequency-input").value
+  console.log("water every "+frequency + " days")
+
+  // moment stuff
+  
+  var today= moment().format('LL')
+  console.log(today)
+
+  var daysSinceLastWatering = moment().diff(moment(lastWatering), 'days')
+  console.log(daysSinceLastWatering+ " days since last watering")
+
+  var remainder= daysSinceLastWatering % frequency
+  console.log("remainder"+remainder)
+  
+  var daysTillNextWatering = frequency - remainder
+
+  var nextWaterDate = moment().add(daysTillNextWatering, 'days')
+
 
     //THIS FUNCTION ADDS THE INPUT TO THE DATABASE
     database.ref().push({
-        //saving these values in firebase under the names on the left of the colon
-        Name: name,
-        LastWatered: lastWatering,
-        howOften: frequency,
-        // NextWatering: nextWatering,
+
+      //saving these values in firebase under the names on the left of the colon
+      Name: name,
+      LastWatered: lastWatering,
+      howOften: frequency,
+      NextWatering: moment(nextWaterDate).format('LL'),
+      TimeTillNextWatering: daysTillNextWatering,
     })
-  })
+
+    $(".form").empty()
+})
 
 
-  //THIS FUNCITON PULLS THE VALUES FROM FIREBASE
-  database.ref().on('child_added', function(snapshot){
+//THIS FUNCITON PULLS THE VALUES FROM FIREBASE
+database.ref().on('child_added', function (snapshot) {
 
-    const child = snapshot.val();
-    console.log("This is the snapshot", child);
-     
-      makerow(child)
-  })
+  const child = snapshot.val();
+  console.log("This is the snapshot", child);
+
+  makerow(child)
+})
 
 //THIS FUNCTION MAKES THE NEW ROW WITH THE INFO FROM THE USER
-  function makerow(child){
-    
-        var newRow= $('<tr>').append(
-        $('<td>').text(child.Name),
-         $('<td>').text(child.LastWatered),
-         $('<td>').text(child.howOften),
-         $('<td>').text(child.NextWatering)
-      
-         )
-         
-      $('#infoGoesHere').append(newRow)
+function makerow(child) {
 
-  }
+  var newRow = $('<tr>').append(
+    $('<td>').text(child.Name),
+    $('<td>').text(child.LastWatered),
+    $('<td>').text('once every '+child.howOften +' days'),
+    $('<td>').text(child.NextWatering),
+    $('<td>').text(child.TimeTillNextWatering + ' more days'),
+  )
 
-   
+  $('#infoGoesHere').append(newRow)
+
+}
+
+
 
 
 
